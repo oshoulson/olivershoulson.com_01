cd5a0623d0e516a209f3d4548ea0cbb2ff6b9984
oshoulson <olivershoulson@gmail.com>
2022-03-02 16:55:36 -0500

    Initial Commit

---
+++ dropdown.js
@@ -0,0 +1,6 @@
+const MenuItem = document.querySelector('.primary-item')
+MenuItem.addEventListener('click', () => {
+	const SubNav = document.querySelector('.sub-navigation')
+	SubNav.setAttribute('data-visible', true)
+}
+)// JavaScript Document
